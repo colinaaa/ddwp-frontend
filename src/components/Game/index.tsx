c6313dad740bf8179da7c06d9637c133cf3e54a0
Colin <colinwang.0616@gmail.com>
2020-10-20 23:09:11 +0800

    feat(game): add UI and logic for join room
    there is a know bug relative to mini-program, will try to figure out why and how to solve

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,5 +1,6 @@
-import { View, Button } from '@tarojs/components';
-import Taro, { FC, setNavigationBarTitle, showModal, navigateTo } from '@tarojs/taro';
+import Modal from '@components/Modal';
+import { View, Button, Input } from '@tarojs/components';
+import Taro, { FC, setNavigationBarTitle, showModal, navigateTo, useState, useCallback } from '@tarojs/taro';
 
 import './index.less';
 
@@ -11,25 +12,49 @@ interface Props {
 const Game: FC<Props> = ({ name }) => {
   setNavigationBarTitle({ title: name });
 
-  const handleCreate = () => {
+  const [open, setOpen] = useState(false);
+  const [roomNumber, setRoomNumber] = useState('');
+
+  const handleCreate = useCallback(() => {
     navigateTo({ url: 'create' });
-  };
+  }, []);
+
+  const handleOpen = useCallback(() => {
+    setOpen((open) => !open);
+  }, []);
+
+  const handleConfirm = useCallback(() => {
+    console.log(roomNumber);
+  }, [roomNumber]);
+
+  const handleChange = useCallback((e) => {
+    setRoomNumber(e.detail.value);
+  }, []);
 
   return (
     <View className='game-root'>
+      {open && (
+        <Modal title='搜索房间' open={open} onClose={handleOpen} onConfirm={handleConfirm}>
+          <Input
+            value={roomNumber}
+            type='digit'
+            confirmType='done'
+            maxLength={5}
+            focus={true}
+            placeholder={'请输入房间号'}
+            placeholderClass='game-modal-input-placeholder'
+            className='game-modal-input'
+            onConfirm={handleConfirm}
+            onInput={handleChange}
+            // TODO: transform modal when open input
+          />
+        </Modal>
+      )}
       <Button className='game-btn' onClick={handleCreate}>
         创建房间
       </Button>
 
-      <Button
-        className='game-btn'
-        onClick={() =>
-          // TODO: use own Modal
-          showModal({
-            title: '加入房间',
-          })
-        }
-      >
+      <Button className='game-btn' onClick={handleOpen}>
         加入房间
       </Button>
 
