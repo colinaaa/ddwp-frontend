7241265f7cf67a4f5ade2da078af3beeab121cfb
Colin <colinwang.0616@gmail.com>
2020-11-06 23:36:25 +0800

    feat(config): update config

--- src/config/const.ts
+++ src/config/const.ts
@@ -1,16 +1,30 @@
+import { MutationKey } from '@services/graphql';
+
 const cardNames = ['狼人杀', '谁是卧底', '德国心脏病', '阿瓦隆', 'UNO', ''];
 
-const nameToRoute = {
+const nameToRoute: {
+  [key: string]: string;
+} = {
   UNO: '',
   狼人杀: '/pages/Werewolf/index',
   阿瓦隆: '',
-  谁是卧底: '',
+  谁是卧底: '/pages/Undercover/index',
   德国心脏病: '',
 };
 
-const getRoute = (game: string): string => {
-  return nameToRoute[game];
+const nameToJoinRoom: {
+  [key: string]: MutationKey;
+} = {
+  UNO: 'WEREWOLF_JOIN_ROOM',
+  狼人杀: 'WEREWOLF_JOIN_ROOM',
+  阿瓦隆: 'WEREWOLF_JOIN_ROOM',
+  谁是卧底: 'UNDERCOVER_JOIN_ROOM',
+  德国心脏病: 'WEREWOLF_JOIN_ROOM',
 };
+
+const getRoute = (game: string): string => nameToRoute[game];
+
+const getJoinRoom = (name: string): MutationKey => nameToJoinRoom[name];
 
 const isDev = process.env.NODE_ENV === '"development"';
 
@@ -19,4 +33,4 @@ const GraphQLEndpoint = 'https://ddwp.outsiders.top/graphql';
 
 // const WsEndpoint = 'ws://localhost:4000/ws';
 const WsEndpoint = 'wss://ddwp.outsiders.top/ws';
-export { cardNames, nameToRoute, getRoute, GraphQLEndpoint, WsEndpoint, isDev };
+export { cardNames, nameToRoute, getRoute, getJoinRoom, GraphQLEndpoint, WsEndpoint, isDev };
